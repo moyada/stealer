57528306dbf41c0df4865f4430558aaae5f3a842
xueyikang <victoryutou@gmail.com>
2023-08-30 20:58:41 +0800

    fix kwai

--- core/config.py
+++ core/config.py
@@ -2,7 +2,8 @@ import os
 
 base_path = os.getcwd() + "/video/"
 
-web_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+web_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+sec_ua = "\"Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"115\", \"Chromium\";v=\"115\""
 user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
 
 dy_cookie = "douyin.com; ttwid=1%7C2t7weTC_PXz-5YsEwFiIWrTsKbb-ofQf_d4pqV3RfQQ%7C1671718672%7C9f5fa39af7bc359b652a4e3983dbcc9f64ff90e326ca6f4bc4c14b4b391d108b; home_can_add_dy_2_desktop=%220%22; passport_csrf_token=a876b73c47bf71ca5464da7995a3d691; passport_csrf_token_default=a876b73c47bf71ca5464da7995a3d691; s_v_web_id=verify_lbz64xig_WyCMMP1j_MUQj_4MJW_9PlI_9DpyUldzBh7c; ttcid=4f56fcd65492490bb79931fb4cc931e924; __ac_nonce=063a4884b00f300d79aff; __ac_signature=_02B4Z6wo00f01-VJjTAAAIDAcZ.WP0iE1YvlaYmAAJsACk5wlkDAL7nvdKXGowoPHpzWgZ54lFkNZ.Pk8B8qn6zYN60KwvIxcbFJlBIoSfpSrGZ9eiEehg1LUio7So63NjpHLSHw8mKIbY.s38; strategyABtestKey=%221671727182.549%22; msToken=hU-kCda4wwb8MRK675j6xC4Z2ODnUx5op1HNB5j7P39ykbN-bJHXUC_IptnqN6c5b3z0h9HOtXJcs9wiNCeKijDF1f0Nn8ikyyewk1z2XOrVnEnQLZmTlsjtVVN5Do4=; tt_scid=AlilgWPKS1Vo6GkA-VdfErKJIH8y6sP.T9rLAmEp139iClbUD2-F8VjETj.eyXO87999; msToken=QXw6bfeLSgRUHRonn0xm81uio-a0DOFbHRnytcKTjcKNX9GXfVxaLj_dJZ8LnWE9mS3JgXbRUOdBT1lwGNzfGMHVYBdF2kTSClby_40UY9wAyBfDskxQDQOq9UIoZpc=; download_guide=%223%2F20221223%22"
